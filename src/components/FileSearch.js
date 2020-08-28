4ad86cd10bfff7f5aca48a730935cb8279078aa4
ergouwang <330035385@qq.com>
2020-08-28 23:55:11 +0800

    search input 组件样式完成

--- src/components/FileSearch.js
+++ src/components/FileSearch.js
@@ -1,6 +1,6 @@
 import React, {useEffect, useRef, useState} from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
-import { faSearch } from '@fortawesome/free-solid-svg-icons'
+import { faSearch,faTimes } from '@fortawesome/free-solid-svg-icons'
 
 const FileSearch = ({title, onFileSearch}) => {
     const [inputActive, setInputActive] = useState(false);
@@ -43,39 +43,36 @@ const FileSearch = ({title, onFileSearch}) => {
     },[inputActive]);
 
     return (
-        <div className="alert alert-primary">
+        <div className="alert alert-primary d-flex justify-content-between align-items-center">
             {
                 !inputActive &&
-                <div className="d-flex justify-content-between align-items-center">
+                <>
                     <span>{title}</span>
                     <button
                         type="button"
-                        className="btn btn-primary"
+                        className="icon-button"
                         onClick={searchInputActive}
                     >
-                        <FontAwesomeIcon icon={faSearch}
-                                         title="搜索"
-                        />
-
+                        <FontAwesomeIcon icon={faSearch} size="lg" title="搜索" />
                     </button>
-                </div>
+                </>
             }
             {
                 inputActive&&
-                    <div className="row">
-                        <input className="form-control col-8"
+                    <>
+                        <input className="form-control"
                                value={value}
                                onChange={(e)=>{setValue(value = e.target.value)}}
                                ref={inputElement}
                         />
                         <button
                             type="button"
-                            className="btn btn-primary col-4"
+                            className="icon-button"
                             onClick={closeSearch}
                         >
-                            关闭
+                            <FontAwesomeIcon icon={faTimes} size="lg" title="关闭" />
                         </button>
-                    </div>
+                    </>
             }
         </div>
     );
